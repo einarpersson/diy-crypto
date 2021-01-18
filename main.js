9edd5873d5144a07172c46e6ba5c70dd292f6741
Einar Persson <me@einarpersson.com>
2021-01-18 14:23:03 +0100

    cleanup

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 
-
-// Refaktorisera = städa, omorganisera kod UTAN att ändra funktionalitet
 function caesarEncrypt(msg) {
 
-    // 1. Översätta varje tecken till ett nummer
     const charCodes = []
     for (let i = 0; i < msg.length; i++) {
         charCodes.push(msg[i].charCodeAt(0))
     }
 
-    // 2. shifta det numret
     for (let i = 0; i < charCodes.length; i++) {
         charCodes[i] = charCodes[i] + 1
-
     }
 
-    // 3. översätt tillbaka från charCode till string
     let result = ""
     for (let i = 0; i < charCodes.length; i++) {
         result = result + String.fromCharCode(charCodes[i])
